f1e60bb2eb7dd253f133dc661aedf1ccae536804
Vinícius Oliveira Gonçalves <viniciusoligon@outlook.com>
2020-09-15 18:50:01 -0300

    Pulo finalizado

---
+++ Assets/Scripts/Jogador.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jogador : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    public float forcaPulo;
+
+    public LayerMask layerChao;
+
+    private bool estaNoChao;
+
+    public float distanciaMinimaChao = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Pular();
+        }
+    }
+    void Pular()
+    {
+        if (estaNoChao)
+            rb.AddForce(Vector2.up * forcaPulo);
+    }
+    private void FixedUpdate()
+    {
+        estaNoChao = Physics2D.Raycast(transform.position, Vector2.down, distanciaMinimaChao, layerChao);
+    }
+}
